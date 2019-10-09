87c6187ffda5149561af918ade1e06e3b2afdb4e
Starship87 <55468638+Starship87@users.noreply.github.com>
2019-10-08 19:29:48 -0500

    Add files via upload

--- mastermind tkinter.py
+++ mastermind tkinter.py
@@ -11,28 +11,34 @@ count_whites = 0
 count_blacks = 0
 guess_count = 0
 current_guess = [''] * 4
+guess_history = []
 
 def new_code():
     global code
     while len(code) <4:
         r = random.randint(0,5)
-        if r not in code:
-            code.append(r)
+        if COLORS[r] not in code:
+            code.append(COLORS[r])
+            print(code)
+            
 def score_guess(guess):
     global code, count_whites, count_blacks, guess_count
     count_whites = 0
     count_blacks = 0
     
     for i in range(4):
-        if guess[i] == str(code[i]):
+        if guess[i] == code[i]:
             count_whites += 1
         else:
             for j in range(4):
-                if guess[i] == str(code[j]):
+                if guess[i] == code[j]:
                     count_blacks += 1
                 
                 
     print("whites:" + str(count_whites) +" blacks: " +str(count_blacks))
+    guess_history[-1].append(count_whites)
+    guess_history[-1].append(count_blacks)
+'''
     if count_whites == 4:
         print("congradulations you win")
         print("it took you " +str (guess_count) + "guesses")
@@ -40,7 +46,7 @@ def score_guess(guess):
         print("you lose. The CIA will not hire you")
     else:
         player_guess()
-
+'''
         
     
 def player_guess():
@@ -72,12 +78,44 @@ def close_color_picker(pos, btn, newcolor):
     color_picker.destroy()
     btn.config(bg=newcolor)
     current_guess[pos] = newcolor
+def submit_guess():
+    global guess_count
+    if ''not in current_guess:
+        guess_history.append(current_guess.copy())
+    #print(guess_history)
+    guess_count +=1
+    #insert scoring here
+    score_guess(current_guess)
+    draw()
     
    
 def draw():
     global canvas
     canvas.delete(tkinter.ALL)
-    canvas.create_rectangle(0,0,cwidth, cheight, fill="lime")
+    canvas.create_rectangle(0,0,cwidth, cheight, fill="grey")
+    xpos=10
+    ypos=10
+    for g in guess_history:
+        counter = 0
+        for clr in g:
+            if counter < 4:
+                canvas.create_oval(xpos,ypos, xpos + 30,ypos + 30, fill=clr)
+                xpos += 40
+            elif counter == 4:
+                #draw whites
+                for w in range(clr):
+                    canvas.create_oval(xpos,ypos, xpos + 20,ypos + 20, fill="white")
+                    xpos += 30
+            elif counter == 5:
+                #draw blacks
+                canvas.create_oval(xpos,ypos, xpos + 20,ypos + 20, fill="black")
+                xpos += 30
+            counter += 1
+       #reset xpos ypos
+        xpos = 10
+        ypos += 45
+        counter = 0
+            
 
 #buttons for tkinter
 b1 = tkinter.Button(root, width=4, height=2, bg="#999999", command=lambda:open_color_picker(0,b1))
@@ -90,7 +128,7 @@ b4 = tkinter.Button(root, width=4, height=2, bg="#999999", command=lambda:open_c
 b4.grid(row=1, column=3)
 
 #submit button
-submit = tkinter.Button(root, text="Submit Guess", font="Arial 16 bold", padx=10)
+submit = tkinter.Button(root, text="Submit Guess", font="Arial 16 bold", padx=10, command=submit_guess)
 submit.grid(row=1, column=4, padx=20)
 
 #canvas
